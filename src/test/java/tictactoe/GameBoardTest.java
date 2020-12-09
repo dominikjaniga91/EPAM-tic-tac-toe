63c6689a6d3fe4b678a46f275dfa850390c92f66
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-09 16:27:08 +0100

    Test the equality of game board

---
+++ src/test/java/tictactoe/GameBoardTest.java
@@ -0,0 +1,30 @@
+package tictactoe;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GameBoardTest {
+
+    GameBoard gameBoard;
+
+    @BeforeMethod
+    public void setUp() {
+        gameBoard = new GameBoard();
+    }
+
+    @Test
+    public void shouldReturnEmptyGameBoard_afterPrint() {
+
+        char[][] actual = {
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '} };
+
+        char[][] expected = gameBoard.getGameBoard();
+
+        Assert.assertEquals(actual, expected);
+    }
+}
