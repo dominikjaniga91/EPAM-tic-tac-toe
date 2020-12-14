e6352a304a7d4ea5c38f82328672f71c46d3d6a4
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-14 17:37:05 +0100

    Create game class with play method

---
+++ src/main/java/tictactoe/Game.java
@@ -0,0 +1,35 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+class Game {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private final Parser parser = new Parser();
+    private final GameBoard gameBoard = new GameBoard();
+    private final Reader reader = new Reader(scanner);
+    private final Player player = new Player(parser);
+    private final Computer computer = new Computer();
+    private int counter = 0;
+
+    void play() {
+        gameBoard.setUpGameBoard();
+        System.out.println(" Choose game mark (X or O)");
+        String mark = reader.readUserInput();
+        FieldValue playerValue = player.selectTheGameMark(mark);
+        computer.selectTheGameMark(playerValue);
+        gameBoard.printGameBoard();
+        String userInput;
+        while (counter < 9) {
+            System.out.println("Make a move - chose position (format: 00)");
+            userInput = reader.readUserInput();
+            Field userField = player.makeAMove(userInput);
+            gameBoard.setValue(userField);
+            Field computerField = computer.makeAMove();
+            gameBoard.setValue(computerField);
+            gameBoard.printGameBoard();
+            counter++;
+        }
+    }
+
+}
--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -2,6 +2,7 @@ package tictactoe;
 
 class Main {
     public static void main(String[] args) {
-
+        Game game = new Game();
+        game.play();
     }
 }
