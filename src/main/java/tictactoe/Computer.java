5e319cec502e87b7d42a7690422895d2b41c4585
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-14 15:45:39 +0100

    Create make move method in computer class

--- src/main/java/tictactoe/Computer.java
+++ src/main/java/tictactoe/Computer.java
@@ -1,12 +1,21 @@
 package tictactoe;
 
+import java.util.Random;
+
 public class Computer {
 
     private FieldValue fieldValue;
+    private final Random random = new Random();
 
     void selectTheGameMark(FieldValue playerMark) {
 
         this.fieldValue = FieldValue.X.equals(playerMark) ? FieldValue.O
                                                           : FieldValue.X;
+    }
+
+    Field makeAMove() {
+        int firstPosition = random.nextInt(4);
+        int secondPosition = random.nextInt(4);
+        return new Field(firstPosition, secondPosition, fieldValue);
     }
 }
