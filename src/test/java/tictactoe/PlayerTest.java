36d3503c4ebafdb1723dbfae0be166ab51ba2c4a
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-14 21:28:46 +0100

    Test selecting game mark by player

---
+++ src/test/java/tictactoe/PlayerTest.java
@@ -0,0 +1,26 @@
+package tictactoe;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PlayerTest {
+
+    private Player player;
+
+    @BeforeMethod
+    public void setUp() {
+        Parser parser = new Parser();
+        player = new Player(parser);
+    }
+
+
+    @Test
+    public void shouldReturnFieldValue_afterSelectGameMark() throws InvalidInputException {
+
+        FieldValue expected = FieldValue.X;
+        FieldValue actual = player.selectTheGameMark("X");
+        Assert.assertEquals(actual, expected);
+    }
+
+}
