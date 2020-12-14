1a000640f6e3ce3af5c0d66dc801f1dcb958b93a
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-14 17:22:04 +0100

    Refactor game board class
    Replace immutable List with ArrayList, and
    create printGameBoard method

--- src/main/java/tictactoe/GameBoard.java
+++ src/main/java/tictactoe/GameBoard.java
@@ -1,20 +1,42 @@
 package tictactoe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class GameBoard {
 
-    private List<Field> gameBoard = List.of( new Field(0, 0, FieldValue.EMPTY), new Field(0, 1, FieldValue.EMPTY), new Field(0, 2, FieldValue.EMPTY),
-                                            new Field(1, 0, FieldValue.EMPTY), new Field(1, 1, FieldValue.EMPTY), new Field(1, 2, FieldValue.EMPTY),
-                                            new Field(2, 0, FieldValue.EMPTY), new Field(2, 1, FieldValue.EMPTY), new Field(2, 2, FieldValue.EMPTY));
+    private List<Field> gameBoard = new ArrayList<>();
 
+    void setUpGameBoard() {
+
+        gameBoard.add(new Field(0, 0, FieldValue.EMPTY));
+        gameBoard.add(new Field(0, 1, FieldValue.EMPTY));
+        gameBoard.add(new Field(0, 2, FieldValue.EMPTY));
+        gameBoard.add(new Field(1, 0, FieldValue.EMPTY));
+        gameBoard.add(new Field(1, 1, FieldValue.EMPTY));
+        gameBoard.add(new Field(1, 2, FieldValue.EMPTY));
+        gameBoard.add(new Field(2, 0, FieldValue.EMPTY));
+        gameBoard.add(new Field(2, 1, FieldValue.EMPTY));
+        gameBoard.add(new Field(2, 2, FieldValue.EMPTY));
+    }
+
+    void printGameBoard() {
+
+        for (int i = 0; i < gameBoard.size(); i++) {
+
+            System.out.printf("[%s]", gameBoard.get(i));
+            if ((i + 1) % 3 == 0) {
+                System.out.println();
+            }
+        }
+    }
 
     boolean setValue(Field userField) {
 
         Field gameBoardField = findField(userField);
 
         if (gameBoardField.isEmpty()) {
-           gameBoardField = userField;
+            gameBoardField.setFieldValue(userField);
             return true;
         }
         return false;
