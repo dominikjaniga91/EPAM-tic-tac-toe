5f80a74fc8f7a4c1bfdc7f9fd44c09072592fe90
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-15 17:26:07 +0100

    Add print error method to Console class

--- src/main/java/tictactoe/Console.java
+++ src/main/java/tictactoe/Console.java
@@ -5,4 +5,9 @@ public class Console {
     static void print(Object message) {
         System.out.println(message);
     }
+
+    static void printError(Object message) {
+        System.err.println(message);
+    }
+
 }
