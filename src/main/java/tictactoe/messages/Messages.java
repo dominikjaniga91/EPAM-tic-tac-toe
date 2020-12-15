17e1532bb4b5495038007c870f85e29691369d92
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-15 21:33:37 +0100

    Create enum with messages

---
+++ src/main/java/tictactoe/messages/Messages.java
@@ -0,0 +1,20 @@
+package tictactoe.messages;
+
+public enum Messages {
+
+    GREETING("Here's the Tic Tac Toe game, enjoy!"),
+    USER_PROMPT("Make a move - chose position (format: 00)"),
+    GAME_BOARD("Actual game board: ");
+
+
+    private final String value;
+
+    Messages(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
