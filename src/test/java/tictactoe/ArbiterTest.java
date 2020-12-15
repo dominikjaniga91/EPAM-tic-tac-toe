f412742643a1ba7fc56768a3412e137f393b9880
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-12-15 15:22:03 +0100

    Create test for Arbiter's check row method

---
+++ src/test/java/tictactoe/ArbiterTest.java
@@ -0,0 +1,44 @@
+package tictactoe;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ArbiterTest {
+
+    private GameBoard gameBoard;
+    private Arbiter arbiter;
+
+    @BeforeMethod
+    public void setUp() {
+        gameBoard = new GameBoard(3);
+        gameBoard.setUpGameBoard();
+        arbiter = new Arbiter(gameBoard);
+    }
+
+    @Test
+    void shouldReturnTrue_afterMarkThreeXInaRow() throws OutOfRangeException {
+
+        Field last = new Field(0, 2, FieldValue.X);
+        gameBoard.setValue(new Field(0, 0, FieldValue.X));
+        gameBoard.setValue(new Field(0, 1, FieldValue.X));
+        gameBoard.setValue(last);
+
+        boolean actual = arbiter.checkRow(last, gameBoard.getGameBoard());
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test
+    void shouldReturnFalse_afterMarkTwoXInaRow() throws OutOfRangeException {
+
+        Field last = new Field(0, 2, FieldValue.X);
+        gameBoard.setValue(new Field(0, 0, FieldValue.X));
+        gameBoard.setValue(new Field(0, 1, FieldValue.O));
+        gameBoard.setValue(last);
+
+        boolean actual = arbiter.checkRow(last, gameBoard.getGameBoard());
+        Assert.assertFalse(actual);
+
+    }
+}
